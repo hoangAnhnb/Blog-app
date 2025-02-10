1903d0efbf7441a77b6679d4923bfeb83ff8bd07
hoangAnhnb <nguyenhoanganhmu48@gmail.com>
2025-02-10 10:18:01 +0700

    complete blog-app

---
+++ backend/lib/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default connectDB;
